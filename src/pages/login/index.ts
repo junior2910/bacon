2e1637d823cc89202a8bcb1aa63bca2db00910b9
Junior Lisboa <juniorzk2910@gmail.com>
2017-08-28 01:00:03 -0300

    login verification

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -1,25 +1,67 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
 
 import { DashboardPage } from '../dashboard/dashboard';
 
+interface User {
+  login:string,
+  senha:string
+}
 @Component({
   selector: 'page-login',
   templateUrl: 'index.html'
 })
 export class Login {
   public isRemember: boolean;
-  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+  public user:User;
+  constructor(
+    public navCtrl: NavController,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+  ) {
 
     this.isRemember = false;
+    this.user = {
+      login: "",
+      senha: ""
+    }
     console.log(this.isRemember);
   }
 
   goToDashboard() {
-    //push another page onto the history stack
-    //causing the nav controller to animate the new page in
-    this.navCtrl.setRoot(DashboardPage);
+    if(this.user.login == 'test@gmail.com' && this.user.senha == '1234') {
+      let loader = this.loadingCtrl.create({
+        content: "Verificando...",
+        duration: 1000
+      });
+
+      loader.onDidDismiss(() => {
+        this.navCtrl.setRoot(DashboardPage);
+      });
+
+      loader.present();
+    } else {
+      let alert = this.alertCtrl.create({
+        subTitle: 'Login ou senha incorretos',
+        buttons: ['OK']
+      });
+
+      let loader = this.loadingCtrl.create({
+        content: "Verificando...",
+        duration: 200
+      });
+
+      loader.onDidDismiss(() => {
+        alert.present();
+      });
+
+      loader.present();
+    }
   }
 
   presentToast() {
