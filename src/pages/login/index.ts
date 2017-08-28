e75958856595fd6463f0c735c5970b122452be3c
Junior Lisboa <juniorzk2910@gmail.com>
2017-08-27 22:05:59 -0300

    login page

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 import {Dashboard} from '../dashboard';
 
@@ -9,12 +10,26 @@ import {Dashboard} from '../dashboard';
 })
 export class Login {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {}
 
   goToDashboard() {
     //push another page onto the history stack
     //causing the nav controller to animate the new page in
     this.navCtrl.push(Dashboard);
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Vamos lembrar de você',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
