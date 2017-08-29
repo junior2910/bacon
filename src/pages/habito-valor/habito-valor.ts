2d1d4be595394761e7a705ca862c1e3b8b278e1d
Junior Lisboa <juniorzk2910@gmail.com>
2017-08-29 01:57:36 -0300

    Storage habitos

--- src/pages/habito-valor/habito-valor.ts
+++ src/pages/habito-valor/habito-valor.ts
@@ -18,6 +18,8 @@ import { LoadingController } from 'ionic-angular';
 })
 export class HabitoValorPage {
   public valorHabito: number;
+  public total:object;
+  public second:object;
   constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController,  private loadingCtrl: LoadingController,) {
   }
 
@@ -25,21 +27,44 @@ export class HabitoValorPage {
     console.log('ionViewDidLoad HabitoValorPage');
   }
 
-  goToSaveHabit(){;
+  goToSaveHabit(){
+
     if(this.valorHabito) {
+
+      localStorage.setItem("valorHabito", this.valorHabito.toString());
 
       let loader = this.loadingCtrl.create({
         content: "Salvando...",
         duration: 1000
       });
 
-      const habitos = [
-        {
-          nome: localStorage.setItem("valorHabito", this.valorHabito.toString());
-        }
-      ];
+      var nome = localStorage.getItem("nomeHabito");
+      var dias = localStorage.getItem("diasHabito");
+      var valor = localStorage.getItem("valorHabito");
+      var totalHabitos = JSON.parse(localStorage.getItem("habitos"));
 
-      localStorage.setItem("valorHabito", this.valorHabito.toString());
+      this.total = [{
+        nome: nome,
+        dias: dias,
+        valor: valor,
+      }]
+
+      this.second = {
+        nome: nome,
+        dias: dias,
+        valor: valor,
+      }
+
+      if(totalHabitos != null) {
+
+        totalHabitos.push(this.second);
+        localStorage.setItem("habitos", JSON.stringify(totalHabitos));
+
+      } else {
+        localStorage.setItem("habitos", JSON.stringify(this.total));
+      }
+
+      console.log(localStorage.getItem("habitos"));
 
       loader.onDidDismiss(() => {
         this.navCtrl.setRoot(DashboardPage);
